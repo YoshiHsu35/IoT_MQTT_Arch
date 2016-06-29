577996807d4990b2283686c81152f4f848768117
YoshiHsu35 <lovekd3535@gmail.com>
2016-06-29 17:47:43 +0800

    IOTSV fixed

--- IoTServer/IoTServer.py
+++ IoTServer/IoTServer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from threading import Thread
+from flask import Flask, jsonify
+from flask import abort
+
+
 
 __author__ = 'Nathaniel'
 
@@ -17,11 +21,12 @@ import config_ServerIPList
 
 _g_cst_ToMQTTTopicServerIP = config_ServerIPList._g_cst_ToMQTTTopicServerIP
 _g_cst_ToMQTTTopicServerPort = config_ServerIPList._g_cst_ToMQTTTopicServerPort
-_g_cst_IoTServerUUID = "IOTSV" #UUID
+_g_cst_IoTServerUUID = "IOTSV-123"
 
 _globalNodeList = []
 _globalFSList = []
 _globalMDList = []
+
 
 print(bcolors.HEADER + "::::::::::::::::::::::::::::::::::::::::::::::::" + bcolors.ENDC)
 print(bcolors.HEADER + "::::::::::::::::::::::::::::::::::::::::::::::::" + bcolors.ENDC)
@@ -36,12 +41,46 @@ print(bcolors.HEADER + "....:::.......::::::..::::::......::::::...:::::" + bcol
 print(bcolors.HEADER + "::::::::::::::::::::::::::::::::::::::::::::::::\n" + bcolors.ENDC)
 
 
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+_gg = 0
+
+@app.route('/nit/iotsv/api/nodes', methods=['GET'])
+def get_tasks():
+    return jsonify({'nodes': _globalNodeList})
+
+def updateIoTNodeList(nodeObj):
+    _globalNodeList.append(nodeObj)
+
+def removeIoTNodeList(nodeObj):
+    for p in _globalNodeList:
+        if (p["NodeName"] == nodeObj.NodeName):
+            _globalNodeList.remove(p)
+
 def main():
-    class_IoTSV_MQTTManager.SubscriberThreading("IOTSV/REG").start()
+
+    class_IoTSV_MQTTManager.SubscriberThreading("IOTSV/REG", updateIoTNodeList, removeIoTNodeList).start()
+    app.run(debug=False,host="0.0.0.0")
 
     # sm = class_MQTTManager.SubscriberManager()
     # sm.subscribe("GW1")
 
 
 if __name__ == '__main__':
+    #_gg=1
     main()
